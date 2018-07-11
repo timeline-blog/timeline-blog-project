9a93552917ce790ee40c0805378f2b6dfa620f95
Logan Mace <lmmace92@hotmail.com>
2018-07-11 09:16:40 -0500

    testing queries

--- db/home/following/getEventByStory.sql
+++ db/home/following/getEventByStory.sql
@@ -1,4 +1,12 @@
-SELECT  e.event_title, e.e_created_on, e.event_id FROM events e
+SELECT  e.event_title, e.e_created_on, e.event_id, s.story_id FROM events e
 INNER JOIN stories s ON e.story_id = s.story_id
 WHERE s.story_id = $1
+LIMIT 1;
+
+SELECT  e.event_title, e.e_created_on, e.event_id, s.story_id FROM events e
+INNER JOIN stories s ON e.story_id = s.story_id
+WHERE s.user_id IN (
+  SELECT f.follower_id FROM user_follows f
+  WHERE f.follower_id = $1
+)
 LIMIT 1;
