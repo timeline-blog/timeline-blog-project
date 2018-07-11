de856e8e6cb40b764d1429c4456a7673f622686d
Owen Ekhator <owenekhator@rocketmail.com>
2018-07-11 09:10:41 -0500

    started home controller

--- server/controllers/homeCtrl.js
+++ server/controllers/homeCtrl.js
@@ -1,16 +1,27 @@
 
 const getHome =(req,res) =>{
   const { user_id } = req.params;
+  
   const home = req.app.get('db').home.following
         home.getStoriesByFollower([user_id])
             .then(story=>{
+              
                 home.getEventByStory([story[0].story_id])
                     .then(event=>{
-                      home.getImageByEvent([events[0].event_id])
-                          .then(image=>{
-                            
-                            res.status(200).json(obj)
-                          })
+                      
+                      if(event[0]){
+                        home.getImageByEvent([event[0].event_id])
+                            .then(image=>{
+                              let obj=[...story,...event,...image]
+                              res.status(200).json(obj)
+                            })
+                        let obj=[...story,...event,...image]
+                        res.status(200).json(obj)
+
+                      } else{          
+                        let obj=[...story]
+                        res.status(200).json(obj)
+                      }
                     })
             })
 }
