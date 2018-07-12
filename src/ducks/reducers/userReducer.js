bcebb71f0a776ca42b003fde522ac40d71f19c2e
manxyrisen <bmanxy@gmail.com>
2018-07-12 13:41:04 -0500

    added storyReducer

--- src/ducks/reducers/userReducer.js
+++ src/ducks/reducers/userReducer.js
@@ -9,18 +9,13 @@ const EDIT_USER = "EDIT_USER";
 export function editUser(display_name, bio, avatar, user_id) {
   return {
     type: EDIT_USER,
-    payload: axios
-      .put(`/api/user/${user_id}`, display_name, bio, avatar)
-      .then(results => {
-        return results;
-      })
-      .catch(err => console.log(err))
+    payload: axios.put(`/api/user/${user_id}`, display_name, bio, avatar)
   };
 }
 
 export default function userReducer(state = initialState, action) {
   switch (action.type) {
-    case `${EDIT_USER}`:
+    case `${EDIT_USER}_FULFILLED`:
       return { ...state };
     default:
       return state;
