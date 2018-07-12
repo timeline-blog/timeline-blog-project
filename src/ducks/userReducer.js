893b8831d8fe21a5c5b771d9ff470e75da290c09
manxyrisen <bmanxy@gmail.com>
2018-07-12 11:41:48 -0500

    added userreducer

--- src/ducks/userReducer.js
+++ src/ducks/userReducer.js
@@ -6,9 +6,23 @@ const initialState = {
 
 const EDIT_USER = "EDIT_USER";
 
-export function editUser(user_id) {
+export function editUser(display_name, bio, avatar, user_id) {
   return {
     type: EDIT_USER,
-    payload: axios.put("/api/user/: user_id", editUser)
+    payload: axios
+      .put(`/api/user/${user_id}`, display_name, bio, avatar)
+      .then(results => {
+        return results;
+      })
+      .catch(err => console.log(err))
   };
+}
+
+export default function userReducer(state = initialState, action) {
+  switch (action.type) {
+    case `${EDIT_USER}`:
+      return { ...state };
+    default:
+      return state;
+  }
 }
