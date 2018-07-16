06ed2281e2494c93d7810d022373dbdbd2ca9a0d
Owen Ekhator <owenekhator@rocketmail.com>
2018-07-16 11:30:32 -0500

    Added user details to auth user reducer

--- src/ducks/reducers/userReducer.js
+++ src/ducks/reducers/userReducer.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+
 const initialState = {
-  authedUser: { }
+  authedUser: []
 };
 
 const EDIT_USER = "EDIT_USER";
+const GET_LOGGEDIN_USER = 'GET_LOGGEDIN_USER'
 
 export function editUser(display_name, bio, avatar, user_id) {
   return {
@@ -13,10 +15,25 @@ export function editUser(display_name, bio, avatar, user_id) {
   };
 }
 
+export function getLoggedInUser(){
+  return {
+      type: GET_LOGGEDIN_USER,
+      payload: axios.get(`/auth/loggedIn`)
+  }
+}
+
 export default function userReducer(state = initialState, action) {
+ // console.log(action.payload)
   switch (action.type) {
     case `${EDIT_USER}_FULFILLED`:
       return { ...state };
+
+    case `${GET_LOGGEDIN_USER}_FULFILLED`:
+    
+      const {data} = action.payload
+      return {
+        ...state, authedUser:data
+       }
     default:
       return state;
   }
