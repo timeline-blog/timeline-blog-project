557480b4c8baf8793d1d9b703a1ae557cb66627a
Owen Ekhator <owenekhator@rocketmail.com>
2018-07-16 21:28:44 -0500

    Added function that allows users add and view images

--- src/components/Story/Story.js
+++ src/components/Story/Story.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
-
+import ImageCompressor from 'image-compressor.js'
 import { getStoryById } from "../../ducks/reducers/storyReducer";
 import Event from "./Event";
 import NewEventModal from "./NewEventModal";
@@ -16,12 +16,77 @@ class Story extends Component {
     this.state = {
       modalMode: "hidden",
       editModalMode: "hidden",
-      editEventModalMode: "hidden"
+      editEventModalMode: "hidden",
+      eventTitle: '',
+      eventDescription: '',
+      images: [],
+      resizedImages: [],
     };
     this.toggleModal = this.toggleModal.bind(this);
     this.toggleEditModal = this.toggleEditModal.bind(this);
     this.toggleEditEventModal = this.toggleEditEventModal.bind(this);
   }
+
+  eventTitleChange=(value)=>{
+    this.setState({eventTitle: value})
+  }
+
+  eventDescriptionChange=(value)=>{
+    this.setState({eventDescription: value})
+  }
+
+
+    _handleImageChange=(e)=>{ 
+        if(this.state.images.length==4){
+            return
+        }
+        let arr = []
+        let id = 7; //the id should come from the story or event
+        let reader = new FileReader()
+        let img =e.target.files[0];
+    //    console.log('normal img ', img)
+    //       let resized = [];
+    //       resized = this.state.resizedImages.slice();
+    //       resized.push(img)
+    //      this.setState({resizedImages: resized})
+    
+
+        let that = this;
+       new  ImageCompressor(img, {
+          quality: .3,//signifies how much quality you want on the photo
+          success(result) {
+          let newArr =   that.state.resizedImages.slice();
+         // console.log('image arr after resize ',result)
+            newArr.push(result)
+           that.setState({
+             resizedImages: newArr
+           })
+          }
+       }) 
+       reader.addEventListener("load",()=>{
+           arr = this.state.images.slice();
+           id++    
+           arr.push({
+             id: id,
+             url: reader.result,
+           })
+           this.setState({
+             
+             images: arr
+           }) 
+       }) 
+        reader.readAsDataURL(img)
+      }
+
+      removeImages=(index)=>{
+          let arr = this.state.images.slice();
+          let arr2 = this.state.resizedImages.slice()
+           arr.splice(index, 1)
+           arr2.splice(index,1)
+           this.setState({images: arr})
+           this.setState({resizedImages: arr2})
+
+      }
 
   componentDidMount() {
     this.props.getStoryById(this.props.match.params.story_id);
@@ -52,7 +117,7 @@ class Story extends Component {
   }
 
   render() {
-    console.log(this.props);
+
     const { story } = this.props;
 
     if (story.events) {
@@ -119,6 +184,15 @@ class Story extends Component {
           <NewEventModal
             modalMode={this.state.modalMode}
             toggleModal={this.toggleModal}
+            _handleImageChange={this._handleImageChange}
+            eventTitleChange={this.eventTitleChange}
+            eventDescriptionChange={this.eventDescriptionChange}
+            removeImages={this.removeImages}
+            images={this.state.images}
+            resizedImages={this.state.resizedImages}
+            title={this.state.title}
+            eventDescription={this.state.eventDescription}
+
           />
 
           <EditEventModal
