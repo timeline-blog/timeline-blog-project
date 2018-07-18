cdc0d36e1be0e439be566f88f813cf926009db3b
Mauricio Flores <contact@maurojflores.com>
2018-07-18 12:06:32 -0500

    NewStoryModal.js limits and counts fields' characters

--- src/components/NewStoryModal.js
+++ src/components/NewStoryModal.js
@@ -7,24 +7,49 @@ export default class NewStoryModal extends Component {
         this.state = {
             storyTitleField: '', 
             storyDescriptionField: '', 
-            selectedCategory: ''
+            selectedCategory: '',
+            titleCharsRemaining: 40,
+            descriptionCharsRemaining: 300
         };
 
+        this.titleMaxChars = 40;
+        this.descriptionMaxChars = 300;
+
         this.switchCategory = this.switchCategory.bind(this);
-        this.changeStoryTitleField = this.changeStoryTitleField.bind(this);
-        this.changeStoryDescriptionField = this.changeStoryDescriptionField.bind(this);
+        this.updateTitleCharsRemaining = this.updateTitleCharsRemaining.bind(this);
+        this.updateDescriptionCharsRemaining = this.updateDescriptionCharsRemaining.bind(this);
+        this.handleTitleChange = this.handleTitleChange.bind(this);
+        this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
     }
 
     switchCategory( category ) {
         this.setState({ selectedCategory: category });
     }
 
-    changeStoryTitleField( value ) {
-        this.setState({ storyTitleField: value });
+    updateTitleCharsRemaining( value ) {
+        this.setState({
+            titleCharsRemaining: this.titleMaxChars - value.length
+        })
     }
-
-    changeStoryDescriptionField( value ) {
-        this.setState({ storyDescriptionField: value });
+    
+    updateDescriptionCharsRemaining( value ) {
+        this.setState({
+            descriptionCharsRemaining: this.descriptionMaxChars - value.length
+        })
+    }
+    
+    handleTitleChange( value ) {
+        this.updateTitleCharsRemaining( value );
+        if ( (this.titleMaxChars - value.length) > 0 ) {
+            this.setState({ storyTitleField: value });
+        }
+    }
+    
+    handleDescriptionChange( value ) {
+        this.updateDescriptionCharsRemaining( value );
+        if ( (this.descriptionMaxChars - value.length) > 0 ) {
+            this.setState({ storyDescriptionField: value });
+        }
     }
 
 
@@ -41,12 +66,24 @@ export default class NewStoryModal extends Component {
                 <div className="modal-body">
                     <div className="field-group">
                         <label htmlFor="">Story Title</label>
-                        <input type="text" className="main-input"/>
+                        <input 
+                            onChange={(e) => this.handleTitleChange(e.target.value)} 
+                            type="text" 
+                            className="main-input"
+                            value={this.state.storyTitleField}/>
+                        <span className="char-counter">{this.state.titleCharsRemaining} characters left</span>
                     </div>
 
                     <div className="field-group">
                         <label htmlFor="">Description</label>
-                        <textarea type="text" rows="3" className="main-input"></textarea>
+                        <textarea 
+                            onChange={(e) => this.handleDescriptionChange(e.target.value)}
+                            type="text" 
+                            rows="3" 
+                            className="main-input"
+                            value={this.state.storyDescriptionField}>
+                        </textarea>
+                        <span className="char-counter">{this.state.descriptionCharsRemaining} characters left</span>
                     </div>
 
                     <div className="field-group">
