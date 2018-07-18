b81b282de10ab08a8fe8e8a482607571ecde0ea5
Owen Ekhator <owenekhator@rocketmail.com>
2018-07-18 18:36:37 -0500

    modified query

--- db/event/addEvent.sql
+++ db/event/addEvent.sql
@@ -1,1 +1,1 @@
-INSERT INTO events ( event_title, event_description, e_created_on, user_id, story_id ) VALUES ($1,$2,$3,$4,$5) RETURNING *;
+INSERT INTO events ( event_title, event_description, e_created_on, user_id, story_id,e_urls ) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *;
