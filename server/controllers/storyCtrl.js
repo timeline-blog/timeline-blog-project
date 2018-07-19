4585462dd33e7f91fc5c145b08d50c9980fe0a9c
Owen Ekhator <owenekhator@rocketmail.com>
2018-07-19 10:02:57 -0500

    modified story controller

--- server/controllers/storyCtrl.js
+++ server/controllers/storyCtrl.js
@@ -9,38 +9,50 @@ const getStory = (req,res) =>{
   story.getStoryById([story_id])
     .then(storys=>{
      
-      story.getEventByStory([storys[0].story_id])
+      story.getEventByStory([story_id])
         .then(events=>{
-          if(events[0]){
-            story.getImagesByEvent([events[0].event_id])
-              .then(images=>{
-                let obj ={
-                  user_id: storys[0].user_id,
-                  story_title: storys[0].story_title,
-                  story_description: storys[0].story_description,
-                  story_category: storys[0].story_category,
-                  like_count: storys[0].like_count,
-                  display_name: storys[0].display_name,
-                  avatar: storys[0].avatar,
-                  events: [...events],
-                  images: [...images]
-                }
-                res.status(200).json(obj)
-              })
-          } else {
-            let obj ={
-              user_id: storys[0].user_id,
-              story_title: storys[0].story_title,
-              story_description: storys[0].story_description,
-              story_category: storys[0].story_category,
-              like_count: storys[0].like_count,
-              display_name: storys[0].display_name,
-              avatar: storys[0].avatar,
-              events: [...events],
-              images: []
-            }
-            res.status(200).json(obj) 
-          }
+          res.status(200).json({
+            user_id: storys[0].user_id,
+            story_title: storys[0].story_title,
+            story_description: storys[0].story_description,
+            story_category: storys[0].story_category,
+            like_count: storys[0].like_count,
+            display_name: storys[0].display_name,
+            avatar: storys[0].avatar,
+            events:[...events]
+          })
+
+        //   if(events[0]){
+
+        //     story.getImagesByEvent([events[0].event_id])
+        //       .then(images=>{
+        //         let obj ={
+        //           user_id: storys[0].user_id,
+        //           story_title: storys[0].story_title,
+        //           story_description: storys[0].story_description,
+        //           story_category: storys[0].story_category,
+        //           like_count: storys[0].like_count,
+        //           display_name: storys[0].display_name,
+        //           avatar: storys[0].avatar,
+        //           events: [...events],
+        //           images: [...images]
+        //         }
+        //         res.status(200).json(obj)
+        //       })
+        //   } else {
+        //     let obj ={
+        //       user_id: storys[0].user_id,
+        //       story_title: storys[0].story_title,
+        //       story_description: storys[0].story_description,
+        //       story_category: storys[0].story_category,
+        //       like_count: storys[0].like_count,
+        //       display_name: storys[0].display_name,
+        //       avatar: storys[0].avatar,
+        //       events: [...events],
+        //       images: []
+        //     }
+        //     res.status(200).json(obj) 
+        //   }
         })
     })
 };
